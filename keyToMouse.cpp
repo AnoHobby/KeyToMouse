5a34119fe7971f15984b169039e73e07f52112f9
hanonymous89 <96468681+hanonymous89@users.noreply.github.com>
2022-09-08 18:26:25 +0900

    Add files via upload

--- keyToMouse.cpp
+++ keyToMouse.cpp
@@ -1,125 +1,265 @@
-﻿
-#include <windows.h>
-namespace ano {
-	inline auto getCursorPos() {
-		POINT pos;
-		GetCursorPos(&pos);
-		return pos;
-	}
-	inline auto setCursorRelativePos(int x, int y) noexcept(true) {
-		auto pos = getCursorPos();
-		SetCursorPos(pos.x + x, pos.y + y);
-		return pos;
-	}
-	inline auto mouseEvent(DWORD flag, DWORD data) {
-		auto pos = ano::getCursorPos();
-		INPUT input;
-		input.type = INPUT_MOUSE;
-		input.mi.dx = pos.x;
-		input.mi.dy = pos.y;
-		input.mi.time = input.mi.dwExtraInfo = 0;
-		input.mi.dwFlags = flag;
-		input.mi.mouseData = data;
-		return  SendInput(1, &input, sizeof(input));
-	}
+﻿#include <windows.h>
+#include <functional>
+#include <unordered_map>
+#include <array>
+#include <thread>
+#include <chrono>
+namespace design {
+	template <class T>
+	class Singleton {
+	protected://getInstanceを実装しなければprivateにする
+		Singleton() = default;
+		~Singleton() = default;
+	public:
+		Singleton(const Singleton&) = delete;
+		Singleton(const Singleton&&) = delete;
+		Singleton& operator=(const Singleton&) = delete;
+		Singleton& operator=(const Singleton&&) = delete;
+	 	static T &getInstance(){
+			static T instance;
+			return instance;
+		};
+	};
 }
-HHOOK hook;
-bool active;
-auto horizon = 0, vertical = 0;
-VOID CALLBACK TimerProc(HWND hwnd, UINT msg, UINT id, DWORD) {
-	if (horizon | vertical) {
-		ano::setCursorRelativePos(horizon, vertical);
-	}
-}
-LRESULT CALLBACK hookProc(int nCode, WPARAM wp, LPARAM lp) {
-	if (!active || nCode < 0) {
-		return CallNextHookEx(hook, nCode, wp, lp);
-	}
-	//#define speed wp == WM_KEYDOWN ? 1 : 0;
-	static auto space = false, shift = false;
-	const auto speed = wp == WM_KEYDOWN ? shift ? 1 : space ? 50 : 10 : 0;
-	switch (((KBDLLHOOKSTRUCT*)lp)->vkCode) {
-	case VK_SHIFT:
-	case VK_RSHIFT:
-	case VK_LSHIFT:
-		if (shift = speed) {
-			horizon /= 10;
-			vertical /= 10;
-		}
-		else {
-			horizon *= 10;
-			vertical *= 10;
-		}
-		break;
-	case VK_SPACE:
-		if (space = speed) {
-			horizon *= 5;
-			vertical *= 5;
-		}
-		else {
-			horizon /= 5;
-			vertical /= 5;
-		}
-		break;
-	case 'G':
-		ano::mouseEvent(MOUSEEVENTF_HWHEEL, speed ? -WHEEL_DELTA : 0);
-		break;
-	case 'H':
-		ano::mouseEvent(MOUSEEVENTF_HWHEEL, speed ? WHEEL_DELTA : 0);
-		break;
-	case 'M':
-		ano::mouseEvent(MOUSEEVENTF_WHEEL, speed ? -WHEEL_DELTA : 0);
-		break;
-	case 'E':
-		ano::mouseEvent(MOUSEEVENTF_WHEEL, speed ? WHEEL_DELTA : 0);
-		break;
-	case 'S':
-		ano::mouseEvent(speed ? MOUSEEVENTF_LEFTDOWN : MOUSEEVENTF_LEFTUP, 0);//こっちにshiftでscrollでもいい
-		break;
-	case 'F':
-		ano::mouseEvent(speed ? MOUSEEVENTF_RIGHTDOWN : MOUSEEVENTF_RIGHTUP, 0);
-		break;
-	case 'D':
-		ano::mouseEvent(speed ? MOUSEEVENTF_MIDDLEDOWN : MOUSEEVENTF_MIDDLEUP, 0);
-		break;
-	case 'Q':
-		PostQuitMessage(0);
-		break;
-	case 'C':
-		active = false;
-		break;
-	case 'J':
-		horizon = -speed;
-		break;
-	case 'L':
-		horizon = speed;
-		break;
-	case 'I':
-		vertical = -speed;
-		break;
-	case 'K':
-		vertical = speed;
-		break;
-	default:
-		return CallNextHookEx(hook, nCode, wp, lp);
-	}
-	return 1;
-}
+namespace library {
+	class Timer {
+	private:
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow) {
-	constexpr auto REGISTER_HOT_KEY_ID = 4649;
-	if (!RegisterHotKey(nullptr, REGISTER_HOT_KEY_ID, MOD_CONTROL, 'M') || (hook = SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, hInstance, 0)) == nullptr)return 0;
-	MSG msg;
-	SetTimer(nullptr, 4649, 1, (TIMERPROC)TimerProc);
-	while (GetMessage(&msg, nullptr, 0, 0)) {
-		if (msg.message == WM_HOTKEY) {
-			//active = !active;//起動と終了
-			active = true;//setwindowhookex//必要な時だけ
+	public:
+		Timer(std::chrono::milliseconds interval, std::function<bool()> call){
+			std::thread(
+				[interval,call] {
+					while (true) {
+						auto start = std::chrono::system_clock::now();
+						if (!call())break;
+						auto waste = std::chrono::system_clock::now() - start;
+						if (waste >= interval)continue;
+						std::this_thread::sleep_for(interval - waste);
+					}
+				}
+			).detach();
 		}
-		DispatchMessage(&msg);
-	}
-	UnhookWindowsHookEx(hook);
-	UnregisterHotKey(nullptr, REGISTER_HOT_KEY_ID);
+	};
+}
+namespace os {
+	enum class MOD {
+		ALT = MOD_ALT
+	};
+	enum class MESSAGE {//windowを想定していないのでdispatchmessageを呼んでいない等、loopはぬけがある
+		HOTKEY
+	};
+	class Message {
+	private:
+		std::unordered_map<MESSAGE, std::function<void()> > exe;
+	public:
+		inline auto &regist(decltype(exe)::key_type msg,decltype(exe)::mapped_type proccess) {
+			exe.emplace(msg,proccess);//後から変えたい場合はinsert_or_assign
+			return *this;
+		}
+		inline auto loop() {
+			const std::unordered_map<decltype(MSG::message), MESSAGE> convert{
+				{WM_HOTKEY,MESSAGE::HOTKEY}
+			};
+			MSG msg;
+			while (GetMessage(&msg,nullptr,0,0)) {
+				if (!convert.count(msg.message)||!exe.count(convert.at(msg.message)))continue;
+				exe.at(convert.at(msg.message))();
+			}
+		}
+		inline auto quit() {
+			PostQuitMessage(0);
+		}
+	};
+	template <int ID ,MOD M, UINT VK >
+	class HotKey {
+	private:
+	public:
+		inline HotKey() {
 
-	return 0;
+		}
+		inline bool regist() const noexcept(RegisterHotKey) {
+			return RegisterHotKey(nullptr,ID,static_cast<UINT>(M), VK);
+		}
+		inline ~HotKey()noexcept(UnregisterHotKey) {
+			UnregisterHotKey(nullptr,ID);
+		}
+	};
+	class Cursor{
+	private:
+		struct Point {
+			long x, y;
+		};
+		
+	public:
+		static constexpr auto DEFAULT_DELTA = WHEEL_DELTA;
+		enum class EVENT {
+			LEFT_UP = MOUSEEVENTF_LEFTUP,
+			LEFT_DOWN = MOUSEEVENTF_LEFTDOWN,
+			RIGHT_UP = MOUSEEVENTF_RIGHTUP,
+			RIGHT_DOWN = MOUSEEVENTF_RIGHTDOWN,
+			MIDDLE_UP = MOUSEEVENTF_MIDDLEUP,
+			MIDDLE_DOWN = MOUSEEVENTF_MIDDLEDOWN,
+			SCROLL_HORIZON = MOUSEEVENTF_HWHEEL,
+			SCROLL_VERTICAL = MOUSEEVENTF_WHEEL
+		};
+		inline Point getPos() {
+			POINT pos;
+			GetCursorPos(&pos);
+			return {pos.x,pos.y};
+		}
+		inline auto setPos(Point pos) {
+			SetCursorPos(pos.x,pos.y);
+		}
+		inline auto occurrence(const EVENT e,const Point pos={0,0},const int data=0) {//オーバーロードした方がいいかも
+			INPUT input;
+			input.type = INPUT_MOUSE;
+			input.mi.dx = pos.x;
+			input.mi.dy = pos.y;
+			input.mi.time = input.mi.dwExtraInfo = 0;
+			input.mi.dwFlags = static_cast<int>(e);
+			input.mi.mouseData = data;
+			return  SendInput(1, &input, sizeof(input));	
+		}
+	};
+	enum class VIRTUAL_KEY {
+		SHIFT=VK_SHIFT,
+		R_SHIFT=VK_RSHIFT,
+		L_SHIFT=VK_LSHIFT,
+		SPACE=VK_SPACE
+	};
+	enum class KEY_STATUS {
+		UP=WM_KEYUP,
+		DOWN= WM_KEYDOWN,
+		SYSTEM_UP= WM_SYSKEYUP,
+		SYSTEM_DOWN= WM_SYSKEYDOWN
+	};
+	//inline auto getKeyState(VIRTUAL_KEY key) {
+	//	return GetAsyncKeyState(static_cast<std::underlying_type<decltype(key) >::type >(key));
+	//}
+	class KeyHook:public design::Singleton<KeyHook>{
+	private:
+		HHOOK hook;
+		std::function<bool(KEY_STATUS,int)> exe;
+		static LRESULT CALLBACK hookProc(int nCode, WPARAM wp, LPARAM lp) {//複数のhookを処理する場合はIDを持たせて任意の処理を呼ぶ。ここでは、一度が確定しているので、無駄なことはしない。
+			if (nCode < 0) {
+				return CallNextHookEx(getInstance().hook, nCode, wp, lp);
+			}
+			const std::unordered_map<decltype(wp), KEY_STATUS> convertStatus{
+				{WM_KEYUP,KEY_STATUS::UP},
+				{WM_KEYDOWN,KEY_STATUS::DOWN},
+				{WM_SYSKEYUP,KEY_STATUS::SYSTEM_UP},
+				{WM_SYSKEYDOWN,KEY_STATUS::SYSTEM_DOWN}
+			};
+			const std::unordered_map<decltype(((KBDLLHOOKSTRUCT*)lp)->vkCode), VIRTUAL_KEY> convertKey{
+				{VK_SHIFT,VIRTUAL_KEY::SHIFT},
+				{VK_LSHIFT,VIRTUAL_KEY::L_SHIFT},
+				{VK_RSHIFT,VIRTUAL_KEY::R_SHIFT},
+				{VK_SPACE,VIRTUAL_KEY::SPACE}
+			};
+			auto keyCode=((KBDLLHOOKSTRUCT*)lp)->vkCode;
+			if (convertKey.count(keyCode)) {
+				keyCode = static_cast<decltype(keyCode)>(convertKey.at(keyCode));
+			}
+			if(getInstance().exe(convertStatus.at(wp), keyCode))return 1;
+			return CallNextHookEx(getInstance().hook, nCode, wp, lp);
+		}
+	public:
+		auto regist(decltype(exe) exe) {
+			this->exe = exe;
+			return (hook = SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, GetModuleHandle(0), 0)) != nullptr;
+		}
+		~KeyHook() {
+			UnhookWindowsHookEx(hook);
+		}
+	};
+
+}
+int WINAPI WinMain(HINSTANCE,HINSTANCE,PSTR,int) {
+	os::HotKey < 4649, os::MOD::ALT, 0x4D/*virual key code m*/ > hotkey;
+	os::Cursor cursor;
+	auto horizon = 0, vertical = 0;
+	os::Message msg;
+	bool slow = false, space = false,active=false;
+	library::Timer(std::chrono::milliseconds(1), [&horizon,&vertical,&cursor] {
+		if (!horizon && !vertical)return true;
+		auto pos=cursor.getPos();
+		pos.x += horizon;
+		pos.y += vertical;
+		cursor.setPos(pos);
+		return true;
+		});
+	if (!hotkey.regist()||!os::KeyHook::getInstance().regist([&cursor, &horizon, &vertical, &slow, &space,&active,&msg](os::KEY_STATUS status, int keyCode)->bool {
+		if (!active)return false;
+		constexpr auto DEFAULT = 10, BOOST = 50, SLOW = 1, BOOST_DELTA = os::Cursor::DEFAULT_DELTA * 2;
+		const auto speed = status == os::KEY_STATUS::DOWN ? space ? BOOST : slow ? SLOW : DEFAULT : 0;
+		const auto closure = [&speed](auto& key, const auto& changed) {
+			return [newSpeed = std::exchange(key, speed) ? DEFAULT : changed](auto& number) {
+				if (!number)return;
+				number = 0 < number ? newSpeed : -newSpeed;
+			};
+		};
+		switch (keyCode) {
+		case static_cast<std::underlying_type<os::VIRTUAL_KEY>::type>(os::VIRTUAL_KEY::SPACE):
+		{
+			if (space && status == os::KEY_STATUS::DOWN)break;
+			const auto checkAssign = closure(space, BOOST);
+			checkAssign(horizon);
+			checkAssign(vertical);
+		}
+		break;
+		case 'A':
+		{
+			if (slow && status == os::KEY_STATUS::DOWN)break;
+			const auto checkAssign = closure(slow, SLOW);
+			checkAssign(horizon);
+			checkAssign(vertical);
+		}
+		break;
+		case 'J':
+			horizon = -speed;
+			break;
+		case 'L':
+			horizon = speed;
+			break;
+		case 'I':
+			vertical = -speed;
+			break;
+		case 'K':
+			vertical = speed;
+			break;
+		case 'S':
+			cursor.occurrence(speed ? os::Cursor::EVENT::LEFT_DOWN : os::Cursor::EVENT::LEFT_UP, cursor.getPos());
+			break;
+		case 'D':
+			cursor.occurrence(speed ? os::Cursor::EVENT::MIDDLE_DOWN : os::Cursor::EVENT::MIDDLE_UP, cursor.getPos());
+			break;
+		case 'F':
+			cursor.occurrence(speed ? os::Cursor::EVENT::RIGHT_DOWN : os::Cursor::EVENT::RIGHT_UP, cursor.getPos());
+			break;
+		case 'E':
+			cursor.occurrence(os::Cursor::EVENT::SCROLL_VERTICAL, cursor.getPos(), space ? BOOST_DELTA : slow ? SLOW : os::Cursor::DEFAULT_DELTA);
+			break;
+		case 'V':
+			cursor.occurrence(os::Cursor::EVENT::SCROLL_VERTICAL, cursor.getPos(), -(space ? BOOST_DELTA : slow ? SLOW : os::Cursor::DEFAULT_DELTA));
+			break;
+		case 'G':
+			cursor.occurrence(os::Cursor::EVENT::SCROLL_HORIZON, cursor.getPos(), -(space ? BOOST_DELTA : slow ? SLOW : os::Cursor::DEFAULT_DELTA));
+			break;
+		case 'H':
+			cursor.occurrence(os::Cursor::EVENT::SCROLL_HORIZON, cursor.getPos(), space ? BOOST_DELTA : slow ? SLOW : os::Cursor::DEFAULT_DELTA);
+			break;
+		case 'C':
+			msg.quit();
+			break;
+		default:
+			return false;
+		}
+		return true;
+		}))return EXIT_SUCCESS;
+	msg
+		.regist(os::MESSAGE::HOTKEY, [&msg,&active] {
+		active = !active;
+		})
+		.loop();
+	return EXIT_SUCCESS;
 }
